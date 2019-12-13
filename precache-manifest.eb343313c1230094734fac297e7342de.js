77991d49c9d18e192959f996527c28b88b80f8b4
Fulvio <fulviocanducci@hotmail.com>
2019-12-13 13:20:02 -0300

    Updates

---
+++ precache-manifest.eb343313c1230094734fac297e7342de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176789d7e7be25e5f42f554ea9b3ea7f",
+    "url": "/canducci-graphql-site/index.html"
+  },
+  {
+    "revision": "f19141928a17cd44f468",
+    "url": "/canducci-graphql-site/static/js/2.c70d5239.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/canducci-graphql-site/static/js/2.c70d5239.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70dd3ad047ff97a9fb3e",
+    "url": "/canducci-graphql-site/static/js/main.badbbfac.chunk.js"
+  },
+  {
+    "revision": "d12779e120f00b32d67d",
+    "url": "/canducci-graphql-site/static/js/runtime-main.e68a4a0f.js"
+  }
+]);
